d455c2b17f08b866f4d50c7b3ff30a81547088fa
Vinko Kodzoman <vinko.kodzoman@fer.hr>
2015-10-08 15:44:06 +0200

    Microposts added to the toy_app.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :microposts
   resources :users
   root 'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
